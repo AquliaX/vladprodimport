d314f0725d2c5e0536092d9ee36b656207eff67d
Artem Matveev <magicsaros@gmail.com>
2019-04-11 20:11:00 +0300

    Refactor product repository with hibernate

--- src/main/java/org/vladimirskoe/project/dao/ProductRepositoryImpl.java
+++ src/main/java/org/vladimirskoe/project/dao/ProductRepositoryImpl.java
@@ -1,54 +1,64 @@
 package org.vladimirskoe.project.dao;
 
-import org.springframework.stereotype.Repository;
-import org.vladimirskoe.project.entity.Product;
-
-import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
+import org.vladimirskoe.project.entity.Product;
 
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
 
-    private List<Product> productList = new ArrayList<>();
-    private int index = 0;
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public ProductRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+
+        Assert.notNull(this.sessionFactory, "Session factory cannot be null");
+    }
 
     public Product addProduct(Product product) {
-        product.setId(++index);
-        productList.add(product);
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(product);
+        session.getTransaction().commit();
         return product;
     }
 
     public Product getProductById(Integer id) {
-        Product value = null;
-        for (Product product : productList) {
-            if (product.getId().equals(id)) {
-                value = product;
-            }
-        }
-        return value;
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Product product = session.get(Product.class, id);
+        session.getTransaction().commit();
+        return product;
     }
 
     public List<Product> getAllProducts() {
-        return productList;
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<Product> products = session.createQuery("from Product", Product.class).list();
+        session.getTransaction().commit();
+        return products;
     }
 
     public Product updateProduct(Integer id, Product product) {
-       // int id = product.getId();
-        Product value = getProductById(id);
-
-        value.setName(product.getName());
-        value.setPrice(product.getPrice());
-        value.setWeight(product.getWeight());
-        value.setType(product.getType());
-        value.setProducer(product.getProducer());
-        value.setHcp(product.getHcp());
-
-        return value;
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        product.setId(id);
+        session.saveOrUpdate(product);
+        session.getTransaction().commit();
+        return product;
     }
 
     public void deleteProduct(Integer id) {
-        Product value = getProductById(id);
-        productList.remove(value);
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Product product = session.load(Product.class, id);
+        session.delete(product);
+        session.getTransaction().commit();
     }
 
 }
