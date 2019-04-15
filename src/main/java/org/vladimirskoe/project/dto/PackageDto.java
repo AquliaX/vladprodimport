8eb2675fcdd0f8d42e80c070636aec5ce401d6fa
Andrew Kaznacheev <aqulia10114@gmail.com>
2019-04-15 17:44:23 +0300

    Create Product and Package DTO's

---
+++ src/main/java/org/vladimirskoe/project/dto/PackageDto.java
@@ -0,0 +1,43 @@
+package org.vladimirskoe.project.dto;
+
+import org.vladimirskoe.project.entity.Product;
+
+public class PackageDto {
+
+    private Integer id;
+    private String name;
+    private Product product;
+    private Integer amount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
